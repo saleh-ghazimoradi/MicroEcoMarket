4f0e58ac80a6f8c8447fa077ed54b6a1519d8fef
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-10-28 14:59:24 +0330

    implement the account service functionality

---
+++ account/service/account.go
@@ -0,0 +1,44 @@
+package service
+
+import (
+	"context"
+	"github.com/saleh-ghazimoradi/MircoEcoMarket/account/domain"
+	"github.com/saleh-ghazimoradi/MircoEcoMarket/account/dto"
+	"github.com/saleh-ghazimoradi/MircoEcoMarket/account/repository"
+	"github.com/segmentio/ksuid"
+)
+
+type AccountService interface {
+	CreateAccount(ctx context.Context, input *dto.Account) (*domain.Account, error)
+	GetAccountById(ctx context.Context, id string) (*domain.Account, error)
+	GetAccounts(ctx context.Context, input *dto.AccountQuery) ([]*domain.Account, error)
+}
+
+type accountService struct {
+	accountRepository repository.AccountRepository
+}
+
+func (a *accountService) CreateAccount(ctx context.Context, input *dto.Account) (*domain.Account, error) {
+	account := &domain.Account{
+		Id:   ksuid.New().String(),
+		Name: input.Name,
+	}
+	if err := a.accountRepository.CreateAccount(ctx, account); err != nil {
+		return nil, err
+	}
+	return account, nil
+}
+
+func (a *accountService) GetAccountById(ctx context.Context, id string) (*domain.Account, error) {
+	return a.accountRepository.GetAccountById(ctx, id)
+}
+
+func (a *accountService) GetAccounts(ctx context.Context, input *dto.AccountQuery) ([]*domain.Account, error) {
+	return a.accountRepository.GetAccounts(ctx, input)
+}
+
+func NewAccountService(accountRepository repository.AccountRepository) AccountService {
+	return &accountService{
+		accountRepository: accountRepository,
+	}
+}
