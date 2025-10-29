26e222bf219d85b1ddce00359ecbcd2136669033
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-10-29 15:02:08 +0330

    improve my catalog dto

--- catalog/dto/catalog.go
+++ catalog/dto/catalog.go
@@ -1,18 +1,14 @@
 package dto
 
 type Catalog struct {
-	Name        string  `json:"name"`
-	Description string  `json:"description"`
-	Price       float64 `json:"price"`
+	Name        string  `json:"name" validate:"required"`
+	Description string  `json:"description" validate:"omitempty"`
+	Price       float64 `json:"price" validate:"required,gt=0"`
 }
 
 type CatalogQuery struct {
-	Limit  uint64 `json:"limit"`
-	Offset uint64 `json:"offset"`
-}
-
-type CatalogSearch struct {
-	Query  string `json:"query"`
-	Limit  uint64 `json:"limit"`
-	Offset uint64 `json:"offset"`
+	Limit  uint64   `json:"limit"`
+	Offset uint64   `json:"offset"`
+	Ids    []string `json:"ids,omitempty"`
+	Query  string   `json:"query,omitempty"`
 }
