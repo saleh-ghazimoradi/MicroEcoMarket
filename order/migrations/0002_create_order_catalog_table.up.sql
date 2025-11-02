de6d12b2afdccfde4a750c1e3785be22bc980331
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-11-02 12:00:53 +0330

    define the order table scripts

---
+++ order/migrations/0002_create_order_catalog_table.up.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS order_catalog (
+    order_id CHAR(27) REFERENCES "order" (id) ON DELETE CASCADE,
+    catalog_id CHAR(27),
+    quantity INT NOT NULL,
+    PRIMARY KEY (catalog_id, order_id)
+)
